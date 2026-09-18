86eb84a83338c6872de345e70ff8ed34d6adbc1b
Amy Smith <asmith@liquibase.com>
2026-09-18 13:23:00 -0500

    WI 393847

---
+++ sql/grants/grants_param_DIT.sql
@@ -0,0 +1,25 @@
+--liquibase formatted sql
+
+--changeset asmith:dit_grants labels:"WI 123456" context:DEV endDelimiter:@
+BEGIN
+  DECLARE v_delete_stmt VARCHAR(200);
+  DECLARE v_insert_stmt VARCHAR(200);
+  DECLARE v_select_group_stmt VARCHAR(400); 
+  DECLARE v_select_user_stmt VARCHAR(400); 
+  DECLARE v_update_stmt VARCHAR(200);
+ 
+  FOR v1 as
+      c1 CURSOR FOR SELECT rtrim(ltrim(CREATOR)) CREATOR,NAME from SYSIBM.systables where CREATOR LIKE '%NYHBEODB%'
+      DO
+		set v_delete_stmt = 'GRANT DELETE ON TABLE '||CREATOR||'.'||NAME ||' to USER "CSAPP1","EEAPP1", "PMAPP1","SHAPP1"';
+	    set v_insert_stmt = 'GRANT INSERT ON TABLE '||CREATOR||'.'||NAME ||' to USER "CSAPP1","EEAPP1", "PMAPP1","SHAPP1"';
+	    set v_select_group_stmt = 'GRANT SELECT ON TABLE '||CREATOR||'.'||NAME ||' to GROUP "HXAAPPL", GROUP "HXAPPL", GROUP "HXETL", GROUP "HXFSTEW", GROUP "HXPRPT", GROUP "HXPTWDEV", GROUP "HXTEST", GROUP "HXUST1", GROUP "HXUTEST", GROUP "HXUTSTUP"'; 
+	    set v_select_user_stmt = 'GRANT SELECT ON TABLE '||CREATOR||'.'||NAME ||' to USER "CSAPP1","EEAPP1","HXETL1","HXRPT1","PMAPP1","SHAPP1","SVC_DAST","SVC_DENC","SVC_DFMS"'; 
+	    set v_update_stmt = 'GRANT UPDATE ON TABLE '||CREATOR||'.'||NAME ||' to USER "CSAPP1","EEAPP1", "PMAPP1","SHAPP1"';
+        execute immediate v_delete_stmt;
+        execute immediate v_insert_stmt;
+        execute immediate v_select_group_stmt;
+        execute immediate v_select_user_stmt;
+        execute immediate v_update_stmt;
+      END FOR;
+END
